35b378e9954a34f138c3c3c9bf773112b12b8205
SHUBHAM BHOITE <shubhambhoite21052002@gmail.com>
2023-04-21 16:14:28 +0530

    update

--- TOPICS/Chapter 4/WhileLoop.c
+++ TOPICS/Chapter 4/WhileLoop.c
@@ -1,2 +1,15 @@
 // Question ==> Print Numbers From 0 to n(User Input) using While Loop
 
+#include<stdio.h>
+int main(){
+    int n;4
+    printf("enter number:");
+    scanf("%d" , &n);
+
+    int i=0;
+    while(i<=n){
+        printf("%d \n", i);
+        i++;
+    }
+    return 0;
+}
